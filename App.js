0addc39cd39c2622b27b62d4866af6f89b355423
Isaque F <isaquefrankli@gmail.com>
2021-06-08 18:38:00 -0300

    first one

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+        source={{ uri: 'https://hacker-news-brasil.web.app/new/1' }}
+        style={{ marginTop: 55 }}
+      />
   );
 }
 
